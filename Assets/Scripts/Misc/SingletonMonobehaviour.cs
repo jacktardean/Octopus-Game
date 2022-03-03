19538c395f990d9a0b858842bd3347a4dc8d783c
jacktardean <jackdean1997@hotmail.co.uk>
2022-03-03 21:52:46 +0000

    First Commit

---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+
+
+    // Protected means it can be inherrited from (as this is a abstract class)
+    // Virtual means it can be overridden
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
