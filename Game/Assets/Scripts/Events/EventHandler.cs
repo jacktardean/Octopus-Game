b4deb273e2d9ec9967eeecfc11571744936b35a5
jacktardean <jackdean1997@hotmail.co.uk>
2022-03-05 20:07:59 +0000

    Addition of event handler

---
+++ Game/Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,39 @@
+﻿public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown);
+
+
+public static class EventHandler
+{
+    // Movement Event variable using the MovementDelegate
+
+    public static event MovementDelegate MovementEvent;
+
+    // Movement Event Call For Publishers
+
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown)
+    {
+        if (MovementEvent!=null)
+        {
+            MovementEvent(inputX, inputY,
+                isWalking, isRunning,
+                isIdle, isCarying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                idleRight, idleLeft, idleUp, idleDown);
+        }
+    }
+}
